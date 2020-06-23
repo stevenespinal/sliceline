0307ee1c05dac235f3b8aee0dcb5e34eef6a7c5d
Steven Espinal <stevenjesusespinal.com>
2020-06-23 16:24:39 -0400

    looped through food arrays and grouped them based of their sections property

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,34 +1,57 @@
-export const foods = [
+// import {Food, FoodLabel} from "../Menu/FoodGrid";
+
+export const foodItems = [
   {
     name: 'Cheese Pizza',
-    img: '/img/pizza.png'
+    img: '/img/pizza.png',
+    section: 'Pizza'
   },
   {
     name: 'Pepperoni Pizza',
-    img: '/img/pizza2.jpeg'
-  },
-  {
-    name: 'Cheese Burger',
-    img: '/img/burger.jpeg'
-  },
-  {
-    name: 'Chicken Fingers',
-    img: '/img/chicken-fingers.jpeg'
+    img: '/img/pizza2.jpeg',
+    section: 'Pizza'
   },
   {
     name: 'Chicken Pizza',
-    img: '/img/chicken-pizza.jpeg'
+    img: '/img/chicken-pizza.jpeg',
+    section: 'Pizza'
   },
   {
-    name: 'French Fries',
-    img: '/img/fries.jpeg'
+    name: 'Veggie Pizza',
+    img: '/img/healthy-pizza.jpeg',
+    section: 'Pizza'
+  },
+  {
+    name: 'Cheese Burger',
+    img: '/img/burger.jpeg',
+    section: 'Sandwich'
   },
   {
     name: 'Gyro',
-    img: '/img/gyro.jpeg'
+    img: '/img/gyro.jpeg',
+    section: 'Sandwich'
   },
   {
     name: 'Sandwich',
-    img: '/img/sandwich.jpeg'
-  }
+    img: '/img/sandwich.jpeg',
+    section: 'Sandwich'
+  },
+  {
+    name: 'French Fries',
+    img: '/img/fries.jpeg',
+    section: 'Sides',
+  },
+  {
+    name: 'Chicken Tenders',
+    img: '/img/chicken-fingers.jpeg',
+    section: 'Sides',
+  },
 ];
+
+export const foods = foodItems.reduce((res, food) => {
+  if (!res[food.section]) {
+    res[food.section] = [];
+  }
+  res[food.section].push(food);
+  return res;
+}, {});
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import {foods} from "../Data/FoodData";
+import { foods} from "../Data/FoodData";
 import {Food, FoodGrid, FoodLabel} from "./FoodGrid";
 
 const MenuStyled = styled.div`
+  // margin: 0px 400px 20px 50px;
+  // height: 1000px;
+  // width: 80%;
   margin: 50px;
 `;
 
@@ -11,14 +14,18 @@ const MenuStyled = styled.div`
 export function Menu() {
   return (
     <MenuStyled>
-      <h1>Menu</h1>
-      <FoodGrid>
-        {foods.map(food => (
-          <Food img={food.img}>
-            <FoodLabel>{food.name}</FoodLabel>
-          </Food>
-        ))}
-      </FoodGrid>
+      {Object.entries(foods).map(([sectionName, foods]) => (
+        <>
+          <h1>{sectionName}</h1>
+          <FoodGrid>
+            {foods.map(food => (
+              <Food img={food.img}>
+                <FoodLabel>{food.name}</FoodLabel>
+              </Food>
+            ))}
+          </FoodGrid>
+        </>
+      ))}
     </MenuStyled>
   )
 }
