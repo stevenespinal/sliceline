e9e8a99098c08b9da9c5e2a7917ea044aad581c2
Steven Espinal <stevenjesusespinal.com>
2020-06-25 23:04:21 -0400

    Completed application, orders send to emails on confirm

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,48 @@
 const functions = require('firebase-functions');
+const {email, password} = require("./config");
+const nodemailer = require("nodemailer");
+const htmlToText = require("nodemailer-html-to-text").htmlToText;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: email,
+    pass: password
+  }
+});
+
+const APP_NAME = "Slice Line";
+mailTransport.use("compile", htmlToText());
+
+exports.sendUserEmail = functions.database
+  .ref('/orders/{pushId}')
+  .onCreate((order) => {
+    sendOrderEmail(order.val());
+  });
+
+
+function sendOrderEmail(order) {
+  const mailOptions = {
+    from: `${APP_NAME} <no_reply@sliceline.com`,
+    to: order.email,
+    subject: `Your order from ${APP_NAME}`,
+    html: `
+      <table style="width: 500px; margin: auto;">
+        <tr>
+            <th>${order.displayName}</th>
+            <th>You ordered some food, here's the confirmation of that order.</th>
+        </tr>
+        ${order.order.map(({name, quantity, price}) => `
+          <tr>
+            <td>${quantity}</td>
+            <td>${name}</td>
+            <td>${price}</td>
+          </tr>
+        `).join("")}
+      </table>
+    `
+  };
+
+  mailTransport.sendMail(mailOptions);
+
+}
